5df77a69cf13b6c6d9ed85f7d2e7e66d5c33e53c
Miguel Jose Balderas Díaz <miguelbd@live.com.mx>
2019-07-30 13:11:42 -0500

    sqlite
    Se agrego archivo para manejo de base de datos sqlite

---
+++ sqlite/sqlman.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+class DataBase:
+
+    def __init__(self, database):
+        self.conn = sqlite3.connect(database)
+        self.curs = conn.cursor()
+
+    def executeSQL(self,sql):
+        """Ejecuta una sentencia ddl"""
+        self.curs.execute(sql)
+    
+    def executeQuery_fetch(self,sql_query)
+        """Ejecuta query regresa los resultados"""
+        self.executeSQL(sql_query)
+        data = self.curs.fetchall()
+        return data
+    
+    def loadData(self,table, data)
+        """Carga los datos en la tabla indicada"""
+        cols_total = len(data[0])
+        self.curs.executemany(get_sqlInsert(table,cols_total),data)
+        self.conn.commit()
+
+    def get_sqlInsert(table, cols_total):
+        sql = "INSERT INTO " + table + " VALUES ("
+        for n in range(0,cols_total):
+            sql += "?,"
+        sql = sql[0:len(sql)-1] + ")"
+        return sql
